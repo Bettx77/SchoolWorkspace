0dcc594769f67a9b64af5d725459d91477253708
Alžběta Rousková <bettyrouskova@gmail.com>
2025-02-10 09:07:33 +0100

    Added 8queens algorithm

---
+++ src/Chess.java
@@ -0,0 +1,66 @@
+class Chess {
+    static final int N = 8;
+
+    // punkce pro kontrolu bezpečnosti umístění dámy
+    static boolean isSafe(int board[][], int row, int col) {
+        // kontrola sloupce
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 1)
+                return false;
+
+        // kontrola levé diagonály
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        // kontrola pravé diagonály
+        for (int i = row, j = col; i >= 0 && j < N; i--, j++)
+            if (board[i][j] == 1)
+                return false;
+
+        return true;
+    }
+
+    // funkce pro umístění dam
+    static boolean solveNQueens(int board[][], int row) {
+        if (row >= N) {
+            printSolution(board);
+            return true; // pro všechna řešení false
+        }
+
+        boolean foundSolution = false; // kontrola najití
+
+        for (int col = 0; col < N; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 1;
+
+                // rekurzivně umístíme další dámu do dalšího řádku
+                foundSolution = solveNQueens(board, row + 1) || foundSolution;
+
+                //  odstraníme dámu a zkoušíme další sloupec
+                board[row][col] = 0;
+            }
+        }
+
+        return foundSolution;
+    }
+
+    // print
+    static void printSolution(int board[][]) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print((board[i][j]));
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int board[][] = new int[N][N]; // inicializace prázdné šachovnice
+
+        if (!solveNQueens(board, 0)) {
+            System.out.println("Řešení neexistuje.");
+        }
+    }
+}
